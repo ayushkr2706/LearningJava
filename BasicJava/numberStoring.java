af5eec59019316a49464adf1ecf9596bf1a28514
ayushkr2706 <ayushkr2706@gmail.com>
2026-08-12 13:48:00 +0530

    TypeCasting in Java

---
+++ BasicJava/numberStoring.java
@@ -0,0 +1,21 @@
+package BasicJava;
+
+public class numberStoring {
+
+    public static void main(String[] args) {
+        
+        float x = 8.125f;
+        float y = 0.7f;
+        // System.out.println(x);
+        System.out.printf("%.20f%n", y); //prints -> 0.6999999999.....upto 20 decimal places.
+
+        // int num = 0X9A; //--> hexadecimal
+        // System.out.println(num);
+
+        byte b = (byte) 0b11010110;
+        System.out.println(b);
+
+
+    }
+    
+}
---
+++ BasicJava/typeCasting.java
@@ -0,0 +1,28 @@
+package BasicJava;
+
+public class typeCasting {
+
+    public static void main(String[] args) {
+        
+
+    // int i = 300;
+    // byte b = (byte) i;
+    // System.out.println(b);
+
+    //Implicit Conversion
+    // byte b = 24;
+    // int i = b;
+    // System.out.println(i);
+
+    // int i = 'c';
+    // System.out.println(i);
+
+
+    //Truncating Conversion
+    // float f = 16.25f;
+    // int i = (int) f;
+    // System.out.println(i);
+
+    }
+    
+}
