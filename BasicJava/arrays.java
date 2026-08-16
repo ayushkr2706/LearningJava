be188bf2f43f65d83392a40c4dc887fab220ca04
ayushkr2706 <ayushkr2706@gmail.com>
2026-08-16 22:18:49 +0530

    Jaggered arrays

--- BasicJava/arrays.java
+++ BasicJava/arrays.java
@@ -25,8 +25,9 @@ public class arrays {
 
         //2D Arrays
         int [][] marks = new int[3][3];
-
         int y = 101;
+
+        //Taking values
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 marks[i][j] = y;
@@ -38,6 +39,34 @@ public class arrays {
         for(int i=0; i<3; i++){
             for(int j=0; j<3; j++){
                 System.out.print(marks[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //Jaggered Arrays
+        int jag[][] = new int[4][];
+        int z = 101;
+        for(int row=0; row<4; row++){
+            if(row % 2 != 0){
+              jag[row] = new int[5];
+            }
+            else {
+               jag[row] = new int[3];
+                }
+
+                for(int col=0; col<jag[row].length; col++){
+                    jag[row][col] = z;
+                    z++;
+                }
+            }
+        
+
+        //Print the jaggered array
+        for(int row=0; row<jag.length; row++){
+            for(int col=0; col<jag[row].length; col++){
+                System.out.print(jag[row][col] + " ");
             }
             System.out.println();
         }
